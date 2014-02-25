46638f7d568fedaa2f47ee86a27474c3ff1c14bc
eszeikial <eszeikial@gmail.com>
2014-02-25 16:42:00 -0500

    Vector stuff
    Starting steering behavior stuff

--- Assets/MyScripts/ProceduralGenerator.cs
+++ Assets/MyScripts/ProceduralGenerator.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class ProceduralGenerator : MonoBehaviour {
 	
 	public GameObject treePrefab;
-	public int worldSize; 
 	private int treeDist = 6;
+
+	public GameObject waterPrefab;
+
+	public int worldSize; 
 	private Random rng;
 
 	//world is set-up. add trees.
 	void Awake()
 	{
 
+		//Setting up trees.
 		int iAdjust = Random.Range (1, 50); //Prevents world from being the same each game.
 		int jAdjust = Random.Range (1, 50); //same deal.
 
@@ -24,14 +28,20 @@ public class ProceduralGenerator : MonoBehaviour {
 
 				//Couldnt get 2d noise to work, So I did 1D noise twice.
 				float NoiseValue = (SimplexNoise.Noise.Generate((float)iMod/100)+SimplexNoise.Noise.Generate((float)jMod/100));
-				if( ((NoiseValue > .4f)&& NoiseValue < .8f) || NoiseValue < -.6f)
+				if( (NoiseValue > .4f && NoiseValue < .8f) || NoiseValue < -.6f)
 				{
 					float xMod = Random.Range(-2,2); //prevents trees from being spawned in a "grid"
 					float zMod = Random.Range(-2,2);
 					GameObject.Instantiate(treePrefab,new Vector3(i+xMod,0,j+zMod),Quaternion.Euler(90,0,0));
-				}	  
+				}
+				else if(NoiseValue > .9f)
+				{
+					GameObject.Instantiate(waterPrefab,new Vector3(i,0,j),Quaternion.Euler (0,0,0));
+				}
 			}
 		}
+
+
 	}
 	
 	// Update is called once per frame
---
+++ Assets/MyScripts/SteeringVehicle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringVehicle : MonoBehaviour {
+
+	protected float vel;
+	protected float acc;
+	protected float maxAcc;
+	protected float maxVel;
+
+	private RaycastHit hit = new RaycastHit[3];
+
+
+	protected void Steer()
+	{
+
+	}
+
+	protected Vector3 Wander()
+	{
+
+	}
+
+	protected Vector3 Seek()
+	{
+
+	}
+
+	protected Vector3 Avoid()
+	{
+
+	}
+
+	protected bool rayTest(int rayNum)
+	{
+		Vector3 vec = transform.forward.normalized * 5;
+		vec *= Quaternion.Euler(0,-15*rayNum,0);
+	}
+
+}
