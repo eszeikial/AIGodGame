2472aa4db58e7b685eff038879834f6c9253862f
eszeikial <eszeikial@gmail.com>
2014-03-04 16:46:42 -0500

    Avoid + obstacle avoidance

--- Assets/MyScripts/SteeringVehicle.cs
+++ Assets/MyScripts/SteeringVehicle.cs
@@ -1,40 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class SteeringVehicle : MonoBehaviour {
 
-	protected float vel;
-	protected float acc;
-	protected float maxAcc;
-	protected float maxVel;
+	private bool debug = true;
 
-	private RaycastHit hit = new RaycastHit[3];
+	//Steering stuff
+	protected float maxAcc; // max amount of force that can be applied
+	protected float maxVel; // max amount of speed the object can have
 
+	protected Vector3 velocity; // current speed and direction blah bl
+	protected Vector3 steeringForce; // force to be applied this frame
+
+	//Raytesting stuff
+	private RaycastHit[] hit = new RaycastHit[3]; // array of raycasts
+	private bool[] whichHit = new bool[3]; // bool array for raycast d
+
+	private const int hitTestDist = 5;
 
 	protected void Steer()
 	{
 
+		//STEER STUFFF
+	}
+
+	private Vector3 DodgeThings()
+	{
+		Vector3 desVel = new Vector3(0,0,0);
+		float[] distFromHit = new float[3];
+		float[] magn = new float[3];
+
+		for(int i = 0; i < 3; i++)
+		{
+			whichHit[i] = false; // Reset it.
+			whichHit[i] = rayTest(i); // set to true if raytest collides
+			
+			//After check determine DV mod
+			if(whichHit[i])
+			{
+				distFromHit[i] = Vector3.Distance(hit[i].point,transform.position);
+				magn[i] = 1 - (distFromHit[i]/hitTestDist);
+				desVel += Avoid(hit[i].point) * magn[i];
+			}
+		}
+
+		return desVel;
 	}
 
 	protected Vector3 Wander()
 	{
-
+		Vector3 desVel = new Vector3(0,0,0);
+		return desVel;
 	}
 
 	protected Vector3 Seek()
 	{
+		Vector3 desVel = new Vector3(0,0,0);
+		return desVel;
 
 	}
 
-	protected Vector3 Avoid()
+	protected Vector3 Avoid(Vector3 target)
 	{
+		Vector3 desVel = new Vector3(0,0,0);
 
+		desVel = target - transform.position;
+		desVel = Quaternion.Euler(0,180,0) * desVel;
+
+		return desVel;
 	}
 
-	protected bool rayTest(int rayNum)
+	private bool rayTest(int rayNum)
 	{
-		Vector3 vec = transform.forward.normalized * 5;
-		vec *= Quaternion.Euler(0,-15*rayNum,0);
+		Vector3 vec = transform.forward.normalized * hitTestDist;
+		vec = Quaternion.Euler(0,-15+15*rayNum,0) * vec;
+
+		if(Physics.Raycast(transform.position,vec,out hit[rayNum]))
+		{
+			if(debug)
+				Debug.DrawRay(transform.position,vec,Color.red);
+			return true;
+		}
+		else
+		{
+			if(debug)
+				Debug.DrawRay(transform.position,vec,Color.green);
+			return false;
+		}
 	}
 
 }
