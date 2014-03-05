1de53a60ea49157cbaf9274b441f551d24934d0c
eszeikial <eszeikial@gmail.com>
2014-03-05 14:29:01 -0500

    Fixed perlin noise generation.
    No longer using Noise.cs
    using built in mathf.perlinnoise

--- Assets/MyScripts/ProceduralGenerator.cs
+++ Assets/MyScripts/ProceduralGenerator.cs
@@ -4,44 +4,42 @@ using System.Collections;
 public class ProceduralGenerator : MonoBehaviour {
 	
 	public GameObject treePrefab;
-	private int treeDist = 6;
-
-	public GameObject waterPrefab;
-
 	public int worldSize; 
+	private int treeDist = 6;
 	private Random rng;
 
 	//world is set-up. add trees.
 	void Awake()
 	{
 
-		//Setting up trees.
-		int iAdjust = Random.Range (1, 50); //Prevents world from being the same each game.
-		int jAdjust = Random.Range (1, 50); //same deal.
+		int Adjust1 = Random.Range (1, 50); //Prevents world from being the same each game.
+		int Adjust2 = Random.Range (1, 50); //same deal.
+		int Adjust3 = Random.Range (1, 50);
+		int Adjust4 = Random.Range (1, 50);
 
-		for (int i = -worldSize; i < worldSize; i += treeDist) 
+		//start at worldsize+1 to avoid divide by zero errors.
+		for (float i = -worldSize+1; i < worldSize; i += treeDist) 
 		{
-			for(int j = -worldSize; j < worldSize; j += treeDist)
+			for(float j = -worldSize+1; j < worldSize; j += treeDist)
 			{
-				int iMod = (i+worldSize*iAdjust); 
-				int jMod = (j+worldSize*jAdjust);
+				float iMod1 = ((i+worldSize)*Adjust1)/1000; 
+				float jMod1 = ((j+worldSize)*Adjust2)/1000;
+				float iMod2 = ((i+worldSize)*Adjust3)/1000; 
+				float jMod2 = ((j+worldSize)*Adjust4)/1000;
 
 				//Couldnt get 2d noise to work, So I did 1D noise twice.
-				float NoiseValue = (SimplexNoise.Noise.Generate((float)iMod/100)+SimplexNoise.Noise.Generate((float)jMod/100));
-				if( (NoiseValue > .4f && NoiseValue < .8f) || NoiseValue < -.6f)
+				float noise1 = (Mathf.PerlinNoise(iMod1,jMod1));
+				float noise2 = (Mathf.PerlinNoise(iMod2,jMod2));
+
+				float NoiseValue = (noise1 + noise2)/2;
+				if(NoiseValue > .6)
 				{
 					float xMod = Random.Range(-2,2); //prevents trees from being spawned in a "grid"
 					float zMod = Random.Range(-2,2);
 					GameObject.Instantiate(treePrefab,new Vector3(i+xMod,0,j+zMod),Quaternion.Euler(90,0,0));
-				}
-				else if(NoiseValue > .9f)
-				{
-					GameObject.Instantiate(waterPrefab,new Vector3(i,0,j),Quaternion.Euler (0,0,0));
-				}
+				}	  
 			}
 		}
-
-
 	}
 	
 	// Update is called once per frame
